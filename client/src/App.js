18b791fecb6dd62891bca8a76694684a2eb80d3c
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-09-18 16:56:12 +0200

    set the nav bar

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import NavBar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+export default function NavBar(){
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-lg p-3 mb-5 bg-body rounded">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav ml-auto">
+        <a className="nav-link " aria-current="page" href="#">Login</a>
+        <a className="nav-link" href="#">Cart</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    )
+}
